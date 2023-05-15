56ca51e4369880d0fd2f3f1173dde34ca0ed1d3e
Catalin Banici <catalinbanici95@yahoo.com>
2023-05-15 19:03:26 +0200

    first attempt on filters

--- app/src/components/Filters.js
+++ app/src/components/Filters.js
@@ -1,15 +1,24 @@
 import React from "react";
 
-export default function Filters() {
+export default function Filters({ handleFilters }) {
+  function handleFilterChange(event) {
+    handleFilters(event.target.value);
+  }
+
   return (
     <>
       <input type="search" />
-      <select>
-        <option>Africa</option>
-        <option>America</option>
-        <option>Asia</option>
-        <option>Europe</option>
-        <option>Oceania</option>
+
+      <select name="regionFilters" onChange={handleFilterChange}>
+        <option value="all" disabled selected hidden>
+          Filter by Region
+        </option>
+        <option value="africa">Africa</option>
+        <option value="america">America</option>
+        <option value="asia">Asia</option>
+        <option value="europe">Europe</option>
+        <option value="oceania">Oceania</option>
+        <option value="all">All</option>
       </select>
     </>
   );
--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
 import { Link, useLoaderData } from "react-router-dom";
 import Filters from "../components/Filters";
 
-export default function Home() {
+export default function Home({ handleFilters, filterType }) {
   const countries = useLoaderData();
-  // console.log(countries);
+  const [filteredCountries, setFilteredCountries] = useState(countries);
+
+  console.log("countries", countries);
+  console.log("filtered countries array", filteredCountries);
+
+  useEffect(() => {
+    switch (filterType) {
+      case "africa":
+        setFilteredCountries(
+          countries.filter((country) => country.region === "Africa")
+        );
+        break;
+      case "america":
+        setFilteredCountries(
+          countries.filter((country) => country.region === "America")
+        );
+        break;
+      case "asia":
+        setFilteredCountries(
+          countries.filter((country) => country.region === "Asia")
+        );
+        break;
+      case "europe":
+        setFilteredCountries(
+          countries.filter((country) => country.region === "Europe")
+        );
+        break;
+      case "oceania":
+        setFilteredCountries(
+          countries.filter((country) => country.region === "Oceania")
+        );
+        break;
+    }
+  }, [filterType, countries]);
 
   return (
     <>
       <div>
-        <Filters />
+        <Filters handleFilters={handleFilters} />
       </div>
       <div>
-        {countries.map((country, index) => (
+        {filteredCountries.map((country, index) => (
           <Link key={index} to={`details/${country.name.common}`}>
             <Card country={country} />
           </Link>
